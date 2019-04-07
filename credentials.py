1c905ff2f8879ea3c214830270a58ad0f82e33e2
fuaad001 <husseinfuaad@hotmail.com>
2019-04-07 14:25:30 +0300

    add method to add credentials

--- credentials.py
+++ credentials.py
@@ -13,3 +13,10 @@ class Credentials:
         self.account_name = account_name
         self.account_user_name = account_user_name
         self.account_password = account_password
+
+    def add_credentials(self):
+        '''
+        add_credentials method adds a new account's credentials object into credentials_list
+        '''
+
+        Credentials.credentials_list.append(self)
